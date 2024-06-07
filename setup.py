bf3dcf344ba80c6f26c35bca18bf3f57f1e8255f
Bizzy-0110 <diego.bizzaro06@gmail.com>
2024-06-07 20:50:48 +0200

    Added sftp connection, download and upload methods

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ class Setup():
     # Attributes
     config.load_settings() # Load settings
     sshClient = paramiko.SSHClient() # ssh client
+    sftpClient = None # sftp client
 
     def execute_command(self, command): # Execute a command on the server via ssh
 
@@ -40,15 +41,53 @@ class Setup():
 
     def stop_ssh(self): # Close the ssh connection
 
-        self.sshClient.close()
+        self.sshClient.close() # Close the ssh connection
         print("SSH connection closed")
 
+    def start_sftp(self): # Start the sftp connection
+            
+        self.sftpClient = self.sshClient.open_sftp() # Open the sftp connection
+        print("SFTP connection established")
+
+    def stop_sftp(self): # Close the sftp connection
+            
+        self.sftpClient.close() # Close the sftp connection
+        self.sftpClient = None # Reset the sftp client
+        print("SFTP connection closed")
     
+    def upload_file(self, local_path, remote_path): # Upload a file to the server
+
+        if self.sftpClient is None: # If the sftp connection is not open
+                
+            self.start_sftp()
+        
+        try:
+            self.sftpClient.put(local_path, remote_path)
+            print(f"{local_path} uploaded successfully in to {remote_path}")
+
+        except Exception as e:
+            print("Sftp Error:", str(e))
+    
+    def download_file(self, remote_path, local_path): # Download a file from the server
+
+        if self.sftpClient is None: # If the sftp connection is not open
+                
+            self.start_sftp()
+
+        try:
+
+            self.sftpClient.get(remote_path, local_path)
+            print(f"{remote_path} downloaded successfully in to {local_path}")
+
+        except Exception as e:
+            
+            print("Sftp Error:", str(e))
+
     def main(self): # Main function
 
-        self.start_ssh() # avvio la connessione ssh
+        self.start_ssh() # start the ssh connection
 
-        self.execute_command("echo 'Hello World'") # eseguo il comando
+        self.execute_command("echo 'Hello World'") # execute a command on the server
 
         self.stop_ssh()
 
